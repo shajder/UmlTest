0623882ea18134cfc00a09a899252bc59157a115
Marcin Hajder <marcin.hajder@gmail.com>
2025-03-21 17:16:57 +0100

    bug fix

--- clang_format.sh
+++ clang_format.sh
@@ -2,7 +2,7 @@
 
 MAIN_BRANCH=${1:-"origin/main"}
 CLANG_BIN=${2:-"`which clang-format-14`"}
-COMMAND_OUTPUT=$(git-clang-format --diff MAIN_BRANCH --extensions c,cpp,h,hpp --binary $CLANG_BIN)
+COMMAND_OUTPUT=$(git-clang-format --diff $MAIN_BRANCH --extensions c,cpp,h,hpp --binary $CLANG_BIN)
 grep '^no modified files to format$' <<<"$COMMAND_OUTPUT" && exit 0
 grep '^clang-format did not modify any files$' <<<"$COMMAND_OUTPUT" && exit 0
 echo -e "\n/// code format check failed! \n"
