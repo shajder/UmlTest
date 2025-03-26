a8cfebada906a6cf2bcaf0c6933cdc1d99eb4bd9
Marcin Hajder <marcin.hajder@gmail.com>
2025-03-26 12:28:45 +0100

    clang format fix

--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -5,7 +5,7 @@
 
 TransactionState Transaction::state() { return m_state; }
 
-template <class T> int Transaction::setup(OpType ot, unsigned int src_size) 
+template <class T> int Transaction::setup(OpType ot, unsigned int src_size)
 {
     if (m_state != TS_IDLE) return -1;
 
@@ -26,14 +26,15 @@ template <class T> int Transaction::setup(OpType ot, unsigned int src_size)
     }
     m_dst.resize(sizeof(T) * src_size / div);
 
-    return 0; 
+    return 0;
 }
 
-int Transaction::cleanup() {
+int Transaction::cleanup()
+{
     m_dst.assign(m_dst.size(), 0);
     m_dst.clear();
     m_solver = nullptr;
-    return 0; 
+    return 0;
 }
 
 template <class T>
@@ -51,16 +52,14 @@ template <typename T> int Transaction::result(T*& dst, unsigned int& dst_size)
 {
     if (m_state != TS_DONE) return -2;
     dst = m_dst.data();
-    dst_size = m_dst.size()/sizeof(T);
+    dst_size = m_dst.size() / sizeof(T);
     return 0;
 }
 
-int Transaction::close() {
+int Transaction::close()
+{
     m_state = TS_IDLE;
-    return 0; 
+    return 0;
 }
 
-void Transaction::onNotifyComputeDone() 
-{
-    m_state = TS_DONE;
-}
+void Transaction::onNotifyComputeDone() { m_state = TS_DONE; }
